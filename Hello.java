e27ee9cfcb1f1185bbded2b580d9f09fbca8107e
yuya1021 <yuyatorase@toraseyuuya-no-MacBook-Pro.local>
2014-08-05 23:41:39 +0900

    Add Hello.java
    Add Hello.java

---
+++ Hello.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+
+class Hello{
+
+public static void main(String[] aaa) throws Exception{
+	
+
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	String input = br.readLine();
+	// System.out.println((char)a);
+	System.out.print("Hello " + input);
+		
+
+}
+
+}
